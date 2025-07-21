da5feeb0ad03b6d7ad90e52fa203d5d4a4dafb97
lisa <lisawanjiku41@gmail.com>
2025-07-21 11:23:18 +0300

    js-tdd-writing-unit-tests-lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe("isPalindrome", () => {
+  it("returns true for 'racecar'", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("returns false for 'car'", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  it("is case-insensitive, returns true for 'Mom'", () => {
+    expect(isPalindrome("Mom")).toBe(true);
+  });
+
+  it("returns false for an empty string", () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  it("throws an error for strings with non-alphabetic characters", () => {
+    expect(() => isPalindrome("race-car")).toThrow("Input must only contain alphabetic characters");
+  });
+
+  it("throws an error for non-string input", () => {
+    expect(() => isPalindrome(12321)).toThrow("Input must be a string");
+  });
+});
+
